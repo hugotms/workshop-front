2ae95bd656c060618c7def4504e87a5c8e6918e5
mariuspageot <mariuspageot@gmail.com>
2021-09-28 15:24:52 +0200

    popup style ok

--- zenweb.js
+++ zenweb.js
@@ -1,5 +1,34 @@
-function OpenURL() {
-    chrome.tabs.create({ url: "https://nicolas.brondin-bernard.com" });
-    alert("test");
+let URLC = false;
+const Pourcentage = 57;
+let idUser = localStorage.getItem('idUser');
+
+
+document.getElementById("pour100").innerText = Pourcentage + "%";
+document.getElementById("btnEnSavoirPlus").addEventListener("click", enSavoirPlus);
+document.getElementById("buttonSignaler").addEventListener("click", Signalement);
+
+chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+    let URLs = tabs[0].url;
+    let URL = URLs.split('/');
+    if (!idUser) {
+        idUser = '1';
+        localStorage.setItem('idUser', idUser);
+    }
+
+    if (URLC) {
+        message = URL[0] + '//' + URL[2] + " est conforme."
+    } else if (!URLC) {
+        message = URL[0] + '//' + URL[2] + " n\'est pas conforme."
+        document.getElementById('URLNC').hidden = false;
+    }
+    document.getElementById("URLC").innerText = message;
+});
+
+function enSavoirPlus() {
+    document.getElementById("Signaler").hidden = false;
+    document.getElementById("btnEnSavoirPlus").hidden = true;
 }
-document.getElementById("buttonClose").addEventListener("click", OpenURL);
+
+function Signalement() {
+    alert("le signalement à été effectué.");
+}
